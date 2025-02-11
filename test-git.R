784e1d9302d88f86388382c1e185788c1173a8d3
Jillian Cook <jilliancook@Jillians-MacBook-Pro-3.local>
2025-02-11 15:45:38 -0700

    Second Commit

--- test-git.R
+++ test-git.R
@@ -1,7 +1,12 @@
+# Git add {file/folder}
+# Git commit -m “{commit message}”
+# git push origin main
+
 library(ggplot2)
 
 data(iris)
 head(iris)
 
 ggplot(data = iris, mapping = aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
-  geom_point()
+  geom_point() +
+  geom_line()
